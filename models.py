1e51de8e017a547c52f75425b5670eb62c0ceb38
Naima <59538962+naimataz@users.noreply.github.com>
2020-12-10 16:09:09 -0500

    Update models.py

--- models.py
+++ models.py
@@ -9,13 +9,23 @@ class Task(Base):
     __tablename__ = "tasks"
 
     # add properties here
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable = False) #field is required so cannot be nullable
+    description = Column(String)
+    priority = Column(Integer)
+    due_date = Column(Date)
 
     # add relationships here
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="tasks")
 
 
-class Zoo(Base):
+class User(Base):
     __tablename__ = "zoos"
 
     # add properties here
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable = False)
 
     # add relationships here
+    tasks = relationship("User", back_populates="users") #ONE-TO- MANY relation 
